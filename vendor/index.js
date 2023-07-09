15c6610b3b9167357b98cea4880b99f6e15e55fc
VadimAbyshev <boss.abyshev@gmail.com>
2023-07-09 22:49:26 +0500

    Свайпер

---
+++ vendor/index.js
@@ -0,0 +1,24 @@
+new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      type: 'bullets'
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,        
+      },
+  
+
+  });
